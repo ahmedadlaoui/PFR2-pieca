20e1521e239a50f381c192eaa486670755635667
Ahmed Adlaoui <ahmedadlaoui230@gmaiil.com>
2026-03-20 14:36:45 +0000

    project file structure & docker setup

---
+++ backend/src/main/java/com/pieca/backend/PiecaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pieca.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiecaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiecaBackendApplication.class, args);
+	}
+
+}
