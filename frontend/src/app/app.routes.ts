b8318f9d98961f3af5406ca0013162fac2a766bd
Ahmed Adlaoui <ahmedadlaoui230@gmaiil.com>
2026-03-22 17:48:48 +0100

    landing page / login / register templates

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'login', loadComponent: () => import('./auth/login/login').then(m => m.Login) },
+    { path: 'register', loadComponent: () => import('./auth/register/register').then(m => m.Register) },
+    { path: 'register/:role', loadComponent: () => import('./auth/register/register').then(m => m.Register) }
+];
