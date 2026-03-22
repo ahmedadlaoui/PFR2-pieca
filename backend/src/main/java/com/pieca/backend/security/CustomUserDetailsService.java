59d784c861f99ceaff205e1e7d7a17d97a4c5cfd
Ahmed Adlaoui <ahmedadlaoui230@gmaiil.com>
2026-03-22 13:44:52 +0100

    adding JWT management tools & security filter chain

---
+++ backend/src/main/java/com/pieca/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.pieca.backend.security;
+
+import com.pieca.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String phoneNumber) throws UsernameNotFoundException {
+        return userRepository.findByPhoneNumber(phoneNumber)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + phoneNumber));
+    }
+}
